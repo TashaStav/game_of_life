5fa75e8bcecd902c1168afe205f5f6f43924076f
NataliyaStavtseva <stav.tasha@gmail.com>
2025-07-03 17:46:05 +0300

    fixed paths in imports

--- src/events.ts
+++ src/events.ts
@@ -1,6 +1,6 @@
-import { createEmptyField, createRandomField } from './gameField.js';
-import { renderField } from './gameView.js';
-import { getNextGeneration, areFieldEqual } from './game.js';
+import { createEmptyField, createRandomField } from './gameField';
+import { renderField } from './gameView';
+import { getNextGeneration, areFieldEqual } from './game';
 
 export let intervalId: number | null = null;
 
